49b3074a056062935bc15741dfea4b5b99512cd7
Dennis Schroer <dev@dennisschroer.nl>
2016-07-20 15:17:16 +0200

    Create PolicyUpdateRecord

--- records/policy_update_record.py
+++ records/policy_update_record.py
@@ -1,35 +1,24 @@
 from typing import Any, Tuple
 
-from records.update_record import UpdateRecord
 
-
-class DataRecord(object):
+class PolicyUpdateRecord(object):
     def __init__(self,
                  read_policy: str = None,
                  write_policy: str = None,
-                 owner_public_key: Any = None,
                  write_public_key: Any = None,
                  encryption_key_read: dict = None,
                  encryption_key_owner: bytes = None,
                  write_private_key: Tuple[dict, bytes] = None,
                  info: bytes = None,
-                 data: bytes = None) -> None:
+                 data: bytes = None,
+                 signature: bytes = None
+                 ) -> None:
         self.info = info
         self.read_policy = read_policy
         self.write_policy = write_policy
-        self.owner_public_key = owner_public_key
         self.write_public_key = write_public_key
         self.encryption_key_read = encryption_key_read
         self.encryption_key_owner = encryption_key_owner
         self.write_private_key = write_private_key
         self.data = data
-
-    def update(self, update_record: UpdateRecord):
-        """
-        Update this record with new data from an UpdateRecord
-
-        :param update_record:
-        :type update_record: records.update_record.UpdateRecord
-        :return:
-        """
-        self.data = update_record.data
+        self.signature = signature
