42fa12e9a2a1b8884531128b00eb218b1537eb49
Dennis Schroer <dev@dennisschroer.nl>
2016-07-25 11:37:15 +0200

    Add method to translate a policy in CNF to an access structure

--- utils/attribute_util.py
+++ utils/attribute_util.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+from charm.toolbox.node import BinNode, OpType
 from charm.toolbox.pairinggroup import PairingGroup
 from charm.toolbox.secretutil import SecretUtil
 
@@ -44,3 +45,31 @@ def add_time_period_to_attribute(attribute: str, time_period: int) -> str:
     True
     """
     return ATTRIBUTE_TIME_FORMAT % (time_period, attribute)
+
+
+def translate_policy_to_access_structure(policy: BinNode):
+    """
+    Translate an access policy to an access structure.
+    Example: (ONE AND THREE) OR (TWO AND FOUR) is translated to
+                [['ONE', 'THREE'], ['TWO', 'FOUR']]
+    The access policy is assumed to be in CNF, see https://en.wikipedia.org/wiki/Conjunctive_normal_form
+    :param policy: The policy to translate
+    :return:
+    >>> group = PairingGroup('SS512')
+    >>> util = SecretUtil(group, verbose=False)
+    >>> policy = util.createPolicy('(ONE AND THREE) OR (TWO AND FOUR)')
+    >>> translated = translate_policy_to_access_structure(policy)
+    >>> translated == [['ONE', 'THREE'], ['TWO', 'FOUR']]
+    True
+    """
+    if policy.type == OpType.OR:
+        return translate_policy_to_access_structure(
+            policy.getLeft()) + translate_policy_to_access_structure(policy.getRight())
+    elif policy.type == OpType.AND:
+        left_translated = translate_policy_to_access_structure(policy.getLeft())
+        left = left_translated if policy.getLeft().type == OpType.AND else [left_translated]
+        right_translated = translate_policy_to_access_structure(policy.getRight())
+        right = right_translated if policy.getRight().type == OpType.AND else [right_translated]
+        return left + right
+    else:
+        return [policy.getAttributeAndIndex()]
