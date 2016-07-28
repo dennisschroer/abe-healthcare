f73a35252e65999aa65a0b95984d7efc82bdf226
Dennis Schroer <dev@dennisschroer.nl>
2016-07-28 14:24:13 +0200

    Improve install script even more

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo apt update
 sudo apt install git
 sudo apt install -y python3 python3-pip python3-dev python3-setuptools
@@ -6,18 +7,18 @@ if [ "$(sudo ldconfig -p | grep libpbc)" ]
 then
 wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
 tar -xvzf pbc-0.5.14.tar.gz
-(cd pbc-0.5.14 && ./configure)
+( cd pbc-0.5.14 && ./configure )
 make -C ./pbc-0.5.14
 sudo make install -C ./pbc-0.5.14
 fi
 sudo apt-get install -y openssl libssl-dev
 git clone https://github.com/denniss17/charm.git
-(cd ./charm && git checkout dev && git pull)
+( cd ./charm && git checkout dev && git pull )
 pip3 install -r charm/requirements.txt
-(cd ./charm && ./configure.sh)
+( cd ./charm && ./configure.sh )
 make -C ./charm
 sudo make install -C ./charm
 git clone git@github.com:denniss17/abe-healthcare.git
-(cd ./abe-healthcare && git checkout master && git pull)
+( cd ./abe-healthcare && git checkout master && git pull )
 pip3 install -r abe-healtchare/requirements.txt
 pip3 install -r abe-healtchare/test-requirements.txt
