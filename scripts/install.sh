6b0ccbba1b067fd4085fde3a8546c54b1d2ac47e
Dennis Schroer <dev@dennisschroer.nl>
2016-07-28 14:22:32 +0200

    Improve install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
-sudo apt -qq update
+sudo apt update
 sudo apt install git
 sudo apt install -y python3 python3-pip python3-dev python3-setuptools
 sudo apt install -y libgmp10 libgmp-dev libgmp3c2
+if [ "$(sudo ldconfig -p | grep libpbc)" ]
+then
 wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
 tar -xvzf pbc-0.5.14.tar.gz
 (cd pbc-0.5.14 && ./configure)
 make -C ./pbc-0.5.14
 sudo make install -C ./pbc-0.5.14
+fi
 sudo apt-get install -y openssl libssl-dev
 git clone https://github.com/denniss17/charm.git
 (cd ./charm && git checkout dev && git pull)
