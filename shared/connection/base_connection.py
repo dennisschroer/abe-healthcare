3f08d98961cdacb0d8dacd90ff6a07c42f4d2b74
Dennis Schroer <dev@dennisschroer.nl>
2016-07-29 14:43:59 +0200

    Measure size of objects transfered in UserInsuranceConnection

--- shared/connection/base_connection.py
+++ shared/connection/base_connection.py
@@ -1,10 +1,13 @@
+from pprint import pprint
+
+
 class BaseConnection(object):
     def __init__(self, benchmark: bool = False) -> None:
         self.benchmark = benchmark
         self.benchmarks = dict()  # type: Dict[str, List[int]]
 
     def dump_benchmarks(self):
-        print(self.benchmarks)
+        pprint(self.benchmarks)
 
     def add_benchmark(self, name: str, size: int) -> None:
         if name not in self.benchmarks:
