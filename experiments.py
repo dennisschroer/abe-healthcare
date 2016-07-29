513e21fae7d09ab685fcd538fe2f5ca3f97f2152
Dennis Schroer <dev@dennisschroer.nl>
2016-07-29 16:26:26 +0200

    Encrypt files of different sizes in experiment

--- experiments.py
+++ experiments.py
@@ -1,6 +1,7 @@
 from experiments.file_size_experiment import FileSizeExperiment
+from shared.implementations.rw15_implementation import RW15Implementation
 
 if __name__ == '__main__':
-    experiment = FileSizeExperiment()
+    experiment = FileSizeExperiment(RW15Implementation())
     experiment.setup()
     experiment.run()
