d086cfc43a7fb6d1f31fa8d8bc6108a24f0c1f6e
Dennis Schroer <dev@dennisschroer.nl>
2016-08-24 13:22:32 +0200

    Add documentation to ExperimentsRun and some refactorings

--- experiments/experiment_run.py
+++ experiments/experiment_run.py
@@ -9,7 +9,13 @@ TIMESTAMP_FORMAT = '%Y-%m-%d %H-%M-%S'
 
 
 class ExperimentsRun(object):
+    """
+    An ExperimentsRun defines a single experiment and the amount of times it should be repeated. Furthermore, it is used
+    to keep track of the state of the experiment: which implementation is currently used and which iteration are we running?
+    """
     def __init__(self, experiment: BaseExperiment, amount: int) -> None:
+        self.timings = list()
+
         self.amount = amount
         self.experiment = experiment
         self.current_implementation = None  # type:BaseImplementation
@@ -21,15 +27,27 @@ class ExperimentsRun(object):
 
     @property
     def timestamp(self) -> str:
+        """
+        Gets the timestamp for this experiments run.
+        """
         if self._timestamp is None:
-            self._timestamp = self.current_time()
+            self._timestamp = self.current_time_formatted()
         return self._timestamp
 
-    def current_time(self) -> str:
+    @staticmethod
+    def current_time_formatted() -> str:
+        """
+        Return the current time, formatted as a string
+        :return:
+        """
         return datetime.datetime.now().strftime(TIMESTAMP_FORMAT)
 
     @property
     def device_name(self) -> str:
+        """
+        Gets the device name of the device running these experiments.
+        :return:
+        """
         if self._device_name is None:
             self._device_name = socket.gethostname()
         return self._device_name
