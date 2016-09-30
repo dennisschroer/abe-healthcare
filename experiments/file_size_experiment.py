62dbe861b80bc7f9810beee1ebe336eb514506a5
Dennis Schroer <dev@dennisschroer.nl>
2016-09-30 13:10:37 +0200

    Update remaining experiments

--- experiments/file_size_experiment.py
+++ experiments/file_size_experiment.py
@@ -1,9 +1,7 @@
-from os.path import join
 from typing import List
 
 from experiments.base_experiment import BaseExperiment
 from experiments.experiment_case import ExperimentCase
-from shared.utils.random_file_generator import RandomFileGenerator
 
 
 class FileSizeExperiment(BaseExperiment):
@@ -11,31 +9,20 @@ class FileSizeExperiment(BaseExperiment):
         'setup_authsetup': 'once',
         'register_keygen': 'once'
     }
+    generated_file_sizes = [
+        1,
+        2 ** 10,
+        2 ** 20,
+        10 * (2 ** 20),
+        50 * (2 ** 20),
+        2 ** 30
+    ]
 
     def __init__(self, cases: List[ExperimentCase] = None) -> None:
         if cases is None:
-            cases = list(map(lambda size: ExperimentCase(size, {'file_size': size}),
-                             [
-                                 1,
-                                 2 ** 10,
-                                 2 ** 20,
-                                 10 * (2 ** 20),
-                                 50 * (2 ** 20),
-                                 2 ** 30
-                             ]))
+            cases = list(map(lambda size: ExperimentCase(size, {'file_size': size}), self.generated_file_sizes))
         super().__init__(cases)
-
-    def global_setup(self) -> None:
-        """
-        Setup all implementation and case independent things for this experiment, like generating random input files.
-        This method is only called once for each experiment, namely at the very start.
-        """
-        file_generator = RandomFileGenerator()
-        input_path = self.get_experiment_input_path()
-        for case in self.cases:
-            file_generator.generate(case.arguments['file_size'], 1, input_path, skip_if_exists=True, verbose=True)
 
     def setup(self):
         super().setup()
-        input_path = self.get_experiment_input_path()
-        self.file_name = join(input_path, '%i-0' % self.state.case.arguments['file_size'])
+        self.encrypted_file_size = self.state.case.arguments['file_size']
