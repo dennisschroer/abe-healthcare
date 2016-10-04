f11926da8dfe465c5fad6a105a0d7c6fd9e61989
Dennis Schroer <dev@dennisschroer.nl>
2016-10-04 16:11:10 +0200

    Add a flag for mobile devices to only run the encryption and decryption in each case

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ from experiments.experiments_runner import ExperimentsRunner
 from experiments.file_size_experiment import FileSizeExperiment
 from experiments.policy_size_experiment import PolicySizeExperiment
 
+IS_MOBILE = False
+
 if __name__ == '__main__':
     runner = ExperimentsRunner()
-    runner.run_experiment(BaseExperiment())
-    runner.run_experiment(FileSizeExperiment())
-    runner.run_experiment(PolicySizeExperiment())
-    runner.run_experiment(AuthoritiesAmountExperiment())
+    base_experiment = BaseExperiment()
+    policy_size_experiment = PolicySizeExperiment()
+    authorities_amount_experiment = AuthoritiesAmountExperiment()
+    file_size_experiment = FileSizeExperiment()
+
+    if IS_MOBILE:
+        base_experiment.run_descriptions = {
+            'setup_authsetup': 'once',
+            'register_keygen': 'once'
+        }
+
+    runner.run_experiment(base_experiment)
+    runner.run_experiment(policy_size_experiment)
+    runner.run_experiment(authorities_amount_experiment)
+    runner.run_experiment(file_size_experiment)
