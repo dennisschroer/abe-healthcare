3f74ed411ae5cf3c89070d1a8e3c4f3b15fff948
Dennis Schroer <dev@dennisschroer.nl>
2016-10-04 13:19:19 +0200

    Add type information to sandbox

--- memory_profiler_sandbox.py
+++ memory_profiler_sandbox.py
@@ -1,19 +1,21 @@
+from typing import List
+
 from memory_profiler import profile, memory_usage
 
 
 class Test(object):
-    def __init__(self):
-        self.a = None
+    def __init__(self) -> None:
+        self.a = None  # type: List[int]
 
-    def my_func(self):
+    def my_func(self) -> List[int]:
         a = [1] * (2 * 10 ** 7)
         return a
 
     @profile
-    def profiled_method(self):
+    def profiled_method(self) -> None:
         self.a = self.my_func()
 
-    def run(self):
+    def run(self) -> None:
         u = memory_usage((self.profiled_method, [], {}))
         print(max(u) - min(u), len(u))
 
