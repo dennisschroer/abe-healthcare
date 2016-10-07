96cd82f3ef1783e6750ec1cf64bb89aa302f1a7f
Dennis Schroer <dev@dennisschroer.nl>
2016-10-07 14:14:37 +0200

    Do not run storage and network tests on mobile devices as the results are equal to these on the notebook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from experiments.authorities_amount_experiment import AuthoritiesAmountExperiment
 from experiments.base_experiment import BaseExperiment
+from experiments.enum.measurement_type import MeasurementType
 from experiments.experiments_runner import ExperimentsRunner
 from experiments.file_size_experiment import FileSizeExperiment
 from experiments.policy_size_experiment import PolicySizeExperiment
@@ -18,6 +19,12 @@ if __name__ == '__main__':
             'setup_authsetup': 'once',
             'register_keygen': 'once'
         }
+        # Storage and network are skipped, as they are just the same as on notebook
+        base_experiment.measurement_types = [
+            MeasurementType.timings,
+            MeasurementType.cpu,
+            MeasurementType.memory
+        ]
 
     runner.run_experiment(base_experiment)
     runner.run_experiment(policy_size_experiment)
