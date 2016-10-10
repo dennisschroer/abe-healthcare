51c15749b3798cf2369438672242fa1f75dd7ff8
Dennis Schroer <dev@dennisschroer.nl>
2016-10-10 15:53:06 +0200

    Add UserKeySizeExperiment to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from experiments.base_experiment import BaseExperiment
 from experiments.file_size_experiment import FileSizeExperiment
 from experiments.policy_size_experiment import PolicySizeExperiment
 from experiments.runner.experiments_runner import ExperimentsRunner
+from experiments.user_key_size_experiment import UserKeySizeExperiment
 
 IS_MOBILE = False
 
@@ -10,6 +11,7 @@ if __name__ == '__main__':
     runner = ExperimentsRunner()
     base_experiment = BaseExperiment()
     policy_size_experiment = PolicySizeExperiment()
+    user_key_size_experiment = UserKeySizeExperiment()
     authorities_amount_experiment = AuthoritiesAmountExperiment()
     file_size_experiment = FileSizeExperiment()
 
@@ -21,7 +23,9 @@ if __name__ == '__main__':
         # Storage and network are skipped, as they are just the same as on notebook
         base_experiment.measurement_types_once = []
 
-    runner.run_experiment(base_experiment)
-    runner.run_experiment(policy_size_experiment)
-    runner.run_experiment(authorities_amount_experiment)
-    runner.run_experiment(file_size_experiment)
+    # runner.run_experiment(base_experiment)
+    # runner.run_experiment(policy_size_experiment)
+    runner.run_experiment(user_key_size_experiment)
+    # runner.run_experiment(file_size_experiment)
+    # runner.run_experiment(authorities_amount_experiment)
+    # runner.run_experiment(file_size_experiment)
