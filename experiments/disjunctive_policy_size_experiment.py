7efe531db766aee686b87654dc5e889885366495
Dennis Schroer <dev@dennisschroer.nl>
2016-10-11 15:32:15 +0200

    Add a policy size experiment for DNF (disjunctive normal form) policies
    This is to investigate the performance of TAAC when the policy is in DNF as opposed to CNF

---
+++ experiments/disjunctive_policy_size_experiment.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from experiments.base_experiment import BaseExperiment
+from experiments.experiment_case import ExperimentCase
+
+
+class DisjunctivePolicySizeExperiment(BaseExperiment):
+    run_descriptions = {
+        'setup_authsetup': 'once',
+        'register_keygen': 'once'
+    }
+
+    def __init__(self, cases: List[ExperimentCase] = None) -> None:
+        if cases is None:
+            attribute_pairs = list(map(lambda a: '(%s@AUTHORITY0 AND %s@AUTHORITY1)' % (a, a), [
+                'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE', 'TEN'
+            ]))
+            cases = list(map(
+                lambda size: ExperimentCase("size %d" % (size + 1),
+                                            {'policy': ' OR '.join(attribute_pairs[:size + 1])}),
+                [2 * x for x in range(5)]
+            ))
+        super().__init__(cases)
+
+    def setup(self):
+        super().setup()
+        self.read_policy = self.state.case.arguments['policy']
