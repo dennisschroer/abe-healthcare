60b4f52984e8710bf0812e0c92f41237cd1c9a6d
Dennis Schroer <dev@dennisschroer.nl>
2016-10-12 14:28:15 +0200

    Remove DataUpdateExperiment, as the update now is added to BaseExperiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from experiments.authorities_amount_experiment import AuthoritiesAmountExperiment
 from experiments.base_experiment import BaseExperiment
-from experiments.data_update_experiment import DataUpdateExperiment
 from experiments.disjunctive_policy_size_experiment import DisjunctivePolicySizeExperiment
 from experiments.file_size_experiment import FileSizeExperiment
 from experiments.policy_size_experiment import PolicySizeExperiment
@@ -17,7 +16,6 @@ if __name__ == '__main__':
     user_key_size_experiment = UserKeySizeExperiment()
     authorities_amount_experiment = AuthoritiesAmountExperiment()
     file_size_experiment = FileSizeExperiment()
-    data_update_experiment = DataUpdateExperiment()
 
     if IS_MOBILE:
         base_experiment.run_descriptions = {
@@ -27,10 +25,9 @@ if __name__ == '__main__':
         # Storage and network are skipped, as they are just the same as on notebook
         base_experiment.measurement_types_once = []
 
-    runner.run_experiment(base_experiment)
-    runner.run_experiment(policy_size_experiment)
-    runner.run_experiment(disjunctive_policy_size_experiment)
-    runner.run_experiment(user_key_size_experiment)
-    runner.run_experiment(authorities_amount_experiment)
-    runner.run_experiment(file_size_experiment)
-    runner.run_experiment(data_update_experiment)
+        runner.run_experiment(base_experiment)
+        runner.run_experiment(policy_size_experiment)
+        runner.run_experiment(disjunctive_policy_size_experiment)
+        runner.run_experiment(user_key_size_experiment)
+        runner.run_experiment(authorities_amount_experiment)
+        runner.run_experiment(file_size_experiment)
